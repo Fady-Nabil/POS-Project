a9ecadcb4cdf19667329dcf0d75c71158fc8ea19
fadynabil gerges <fadynabilgerges@gmail.com>
2020-06-02 22:27:23 +0200

    First Version From Project

---
+++ resources/lang/en/site.php
@@ -0,0 +1,82 @@
+<?php
+
+return [
+    'dashboard' => 'Dashboard',
+    'logout' => 'Logout',
+
+    'add' => 'Add',
+    'create' => 'Create',
+    'read' => 'Show',
+    'edit' => 'Edit',
+    'update' => 'Update',
+    'delete' => 'Delete',
+    'search' => 'Search',
+    'show' => 'Show',
+    'loading' => 'Loading',
+    'print' => 'Print',
+
+    'confirm_delete' => 'Confirm Delete',
+    'yes' => 'Yes',
+    'no' => 'No',
+
+    'login' => ' الدخول',
+    'remember_me' => 'Remember Me',
+    'password' => ' Password',
+    'password_confirmation' => 'Password Confirmation',
+
+    'added_successfully' => 'Added Successfully',
+    'updated_successfully' => 'Updated Successfully',
+    'deleted_successfully' => 'Deleted Successfully',
+
+    'no_data_found' => 'No Data Found',
+    'no_records' => 'No Records',
+
+    'clients' => 'Clients',
+    'client_name' => 'Client Name',
+    'phone' => 'Phone',
+    'address' => 'Address',
+    'previous_orders' => ' Previous Orders',
+    'orders' => 'Orders',
+    'add_order' => 'Add Orders',
+    'edit_order' => 'Edit Orders',
+
+    'users' => 'Moderators',
+    'first_name' => 'First Name',
+    'last_name' => 'Last Name',
+    'email' => 'Email',
+    'image' => 'Image',
+    'action' => 'Action',
+
+    'permissions' => 'Permissions',
+
+    'categories' => 'Categories',
+    'all_categories' => 'All Categories',
+    'name' => 'Name',
+    'description' => 'Description',
+    'products_count' => 'Products Count',
+    'related_products' => 'Related Products',
+    'category' => 'Category',
+    'show_products' => 'Show Products',
+    'created_at' => 'Created At',
+
+    'products' => 'Products',
+    'product' => 'Product',
+    'quantity' => 'Quantity',
+    'total' => 'Total',
+    'purchase_price' => 'Purchase Price',
+    'price' => 'Price',
+    'sale_price' => 'Sale Price',
+    'stock' => 'Stock',
+    'profit_percent' => 'Profit',
+
+    'ar' => [
+        'name' => 'الاسم بالغه العربيه',
+        'description' => 'الوصف بالغه العربيه',
+    ],
+
+    'en' => [
+        'name' => 'Name in english',
+        'description' => 'Description in english',
+    ],
+
+];
